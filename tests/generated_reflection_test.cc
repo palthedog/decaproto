c1e52414733d70b12223391597fb1fa0e0eab2e6
Naoki Iwasaki <niwasaki@f13g.com>
2024-07-02 01:44:51 +0900

    Add tests for generated reflection

--- tests/generated_reflection_test.cc
+++ tests/generated_reflection_test.cc
@@ -17,10 +17,14 @@ const FieldDescriptor& FindFieldDescriptor(
             });
 }
 
+// SimpleMesaage fields
 const uint32_t kNumTag = 1;
 const uint32_t kStrTag = 2;
-const uint32_t kEnumTag = 3;
-const uint32_t kMessageTag = 4;
+const uint32_t kEnumValueTag = 3;
+const uint32_t kOtherTag = 4;
+
+// OtherMessage field(s)
+const uint32_t kOtherNumTag = 1;
 
 // Tests generated Descriptor
 // See simple.proto for the definition of SimpleMessage
@@ -39,10 +43,10 @@ TEST(ReflectionTest, DescriptorTest) {
             FindFieldDescriptor(descriptor, kStrTag).GetType());
     EXPECT_EQ(
             FieldType::kEnum,
-            FindFieldDescriptor(descriptor, kEnumTag).GetType());
+            FindFieldDescriptor(descriptor, kEnumValueTag).GetType());
     EXPECT_EQ(
             FieldType::kMessage,
-            FindFieldDescriptor(descriptor, kMessageTag).GetType());
+            FindFieldDescriptor(descriptor, kOtherTag).GetType());
 }
 
 TEST(ReflectionTest, AccessNumTest) {
@@ -82,8 +86,41 @@ TEST(ReflectionTest, StringNotCopiedTest) {
 
     const Reflection* reflection = m.GetReflection();
 
-    // reflection->SetString(&m, kStrTag, "udon");
+    reflection->SetString(&m, kStrTag, "udon");
 
     // Compare pointers to check that both getters don't copy the string
-    // EXPECT_EQ(&m.str(), &reflection->GetString(&m, kStrTag));
+    EXPECT_EQ(&m.str(), &reflection->GetString(&m, kStrTag));
+}
+
+TEST(ReflectionTest, EnumTest) {
+    SimpleMessage m;
+
+    const Reflection* reflection = m.GetReflection();
+
+    // Easy way
+    EXPECT_EQ(0, m.enum_value());
+
+    reflection->SetEnumValue(&m, kEnumValueTag, ENUM_B);
+
+    EXPECT_EQ(ENUM_B, m.enum_value());
+    EXPECT_EQ(ENUM_B, reflection->GetEnumValue(&m, kEnumValueTag));
+}
+
+TEST(ReflectionTest, MessageTest) {
+    SimpleMessage m;
+
+    const Reflection* reflection = m.GetReflection();
+
+    // Easy way
+    EXPECT_FALSE(m.has_other());
+
+    OtherMessage* mut_other = static_cast<OtherMessage*>(
+            reflection->MutableMessage(&m, kOtherTag));
+    mut_other->set_other_num(100);
+
+    EXPECT_EQ(100, m.other().other_num());
+
+    const Message& other = reflection->GetMessage(&m, kOtherTag);
+    const Reflection* other_reflection = other.GetReflection();
+    EXPECT_EQ(100, other_reflection->GetInt32(&other, kOtherNumTag));
 }
