e8e69278ce25c1a7e436837094735373c6ecb6ca
Naoki Iwasaki <niwasaki@f13g.com>
2024-07-01 10:30:11 +0900

    cleanup

--- generator/field.go
+++ generator/field.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bytes"
 	"log"
 	"strings"
-	"text/template"
 
 	descriptor "github.com/golang/protobuf/protoc-gen-go/descriptor"
 )
@@ -151,8 +149,7 @@ func isPrimitiveType(f *descriptor.FieldDescriptorProto) bool {
 		descriptor.FieldDescriptorProto_TYPE_FIXED32,
 		descriptor.FieldDescriptorProto_TYPE_DOUBLE,
 		descriptor.FieldDescriptorProto_TYPE_FLOAT,
-		descriptor.FieldDescriptorProto_TYPE_BOOL,
-		descriptor.FieldDescriptorProto_TYPE_STRING:
+		descriptor.FieldDescriptorProto_TYPE_BOOL:
 		return true
 	}
 	return false
@@ -162,35 +159,31 @@ func processField(ctx *Context, msg_printer *MessagePrinter, f *descriptor.Field
 	type_name_info := getTypeNameInfo(f)
 	// TODO: Replace it with type_name_info so that we can share more logic across
 	// different types of fields
-	cpp_name := type_name_info.cc_raw_type
 	if f.GetLabel() == descriptor.FieldDescriptorProto_LABEL_REPEATED {
 		// repeated
 		if isPrimitiveType(f) {
 			// primitive types
-			addRepeatedPrimitiveField(f, cpp_name, msg_printer)
+			addRepeatedPrimitiveField(f, &type_name_info, msg_printer)
 		} else if f.GetType() == descriptor.FieldDescriptorProto_TYPE_STRING {
 			// string
-			addRepeatedPrimitiveField(f, cpp_name, msg_printer)
+			addRepeatedPrimitiveField(f, &type_name_info, msg_printer)
 		} else if f.GetType() == descriptor.FieldDescriptorProto_TYPE_ENUM {
 			// enum
-			addRepeatedPrimitiveField(f, cpp_name, msg_printer)
+			addRepeatedPrimitiveField(f, &type_name_info, msg_printer)
 		} else {
 			log.Fatal("Unsupported repeated field type", f.GetType(), f.GetName())
 		}
 	} else if f.GetLabel() == descriptor.FieldDescriptorProto_LABEL_OPTIONAL {
 		// optional
-		if isPrimitiveType(f) {
-			// primitive types
-			addPrimitiveField(f, cpp_name, msg_printer)
+		if isPrimitiveType(f) || f.GetType() == descriptor.FieldDescriptorProto_TYPE_ENUM {
+			// primitive types | enum
+			addPrimitiveField(f, &type_name_info, msg_printer)
 		} else if f.GetType() == descriptor.FieldDescriptorProto_TYPE_STRING {
 			// string
-			addStringField(f, cpp_name, msg_printer)
-		} else if f.GetType() == descriptor.FieldDescriptorProto_TYPE_ENUM {
-			// enum
-			addPrimitiveField(f, cpp_name, msg_printer)
+			addStringField(f, &type_name_info, msg_printer)
 		} else if f.GetType() == descriptor.FieldDescriptorProto_TYPE_MESSAGE {
 			// message
-			addMessageField(f, type_name_info, msg_printer)
+			addMessageField(f, &type_name_info, msg_printer)
 		} else {
 			log.Fatal("Unsupported field type", f.GetType(), f.GetName())
 		}
@@ -199,196 +192,130 @@ func processField(ctx *Context, msg_printer *MessagePrinter, f *descriptor.Field
 	}
 }
 
-func addPrimitiveField(f *descriptor.FieldDescriptorProto, cpp_type string, msg_printer *MessagePrinter) {
-	const pri_template = `
-	{{.cpp_type}} {{.pri_name}};
-`
+func holderName(f *descriptor.FieldDescriptorProto) string {
+	return f.GetName() + "__"
+}
 
-	const pub_template = `
-	inline {{.cpp_type}} {{.f_name}}() const {
-	    return {{.pri_name}};
+func addPrimitiveField(f *descriptor.FieldDescriptorProto, type_name_info *TypeNameInfo, msg_printer *MessagePrinter) {
+	args := map[string]string{
+		"raw_type":    type_name_info.cc_raw_type,
+		"ret_type":    type_name_info.cc_ret_type,
+		"arg_type":    type_name_info.cc_arg_type,
+		"holder_name": holderName(f),
+		"f_name":      f.GetName(),
+	}
+	msg_printer.PushPrivate(
+		print("pri_pri",
+			"    {{.raw_type}} {{.holder_name}};\n",
+			args))
+
+	msg_printer.PushPublic(
+		print("pub_pri", `
+	inline {{.ret_type}} {{.f_name}}() const {
+	    return {{.holder_name}};
 	}
 
-	inline void set_{{.f_name}}( {{.cpp_type}} value) {
-	    {{.pri_name}} = value;
+	inline void set_{{.f_name}}( {{.arg_type}} value) {
+	    {{.holder_name}} = value;
 	}
 
 	inline void clear_{{.f_name}}() {
-	    {{.pri_name}} = {{.cpp_type}}();
+	    {{.holder_name}} = {{.raw_type}}();
 	}
-
-`
-
-	pri_tpl, err := template.New("pri").Parse(pri_template)
-	if err != nil {
-		log.Fatal(err)
-	}
-	pub_tpl, err := template.New("pub").Parse(pub_template)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var pri_name = f.GetName() + "__"
-	var buf bytes.Buffer
-	pri_tpl.Execute(&buf, map[string]string{
-		"cpp_type": cpp_type,
-		"pri_name": pri_name,
-	})
-	msg_printer.PushPrivate(buf.String())
-
-	buf.Reset()
-	pub_tpl.Execute(&buf, map[string]string{
-		"cpp_type": cpp_type,
-		"pri_name": pri_name,
-		"f_name":   f.GetName(),
-	})
-	msg_printer.PushPublic(buf.String())
+`,
+			args))
 }
 
-func addStringField(f *descriptor.FieldDescriptorProto, cpp_type string, msg_printer *MessagePrinter) {
-	const pri_template = `
-	{{.cpp_type}} {{.pri_name}};
-`
+func addStringField(f *descriptor.FieldDescriptorProto, type_name_info *TypeNameInfo, msg_printer *MessagePrinter) {
+	args := map[string]string{
+		"raw_type":    type_name_info.cc_raw_type,
+		"ret_type":    type_name_info.cc_ret_type,
+		"arg_type":    type_name_info.cc_arg_type,
+		"holder_name": holderName(f),
+		"f_name":      f.GetName(),
+	}
+	msg_printer.PushPrivate(
+		print("pri_str", "    {{.raw_type}} {{.holder_name}};\n", args))
 
-	const pub_template = `
-	inline {{.cpp_type}} {{.f_name}}() const {
-	    return {{.pri_name}};
+	msg_printer.PushPublic(
+		print("pub_str", `
+	inline {{.ret_type}} {{.f_name}}() const {
+	    return {{.holder_name}};
 	}
 
 	// TODO: Support string_view
-	inline void set_{{.f_name}}(const {{.cpp_type}}& value) {
-	    {{.pri_name}} = value;
+	inline void set_{{.f_name}}({{.arg_type}} value) {
+	    {{.holder_name}} = value;
 	}
 
 	inline void clear_{{.f_name}}() {
-	    {{.pri_name}} = {{.cpp_type}}();
+	    {{.holder_name}} = {{.raw_type}}();
 	}
-`
-
-	pri_tpl, err := template.New("pri").Parse(pri_template)
-	if err != nil {
-		log.Fatal(err)
-	}
-	pub_tpl, err := template.New("pub").Parse(pub_template)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var pri_name = f.GetName() + "__"
-	var buf bytes.Buffer
-	pri_tpl.Execute(&buf, map[string]string{
-		"cpp_type": cpp_type,
-		"pri_name": pri_name,
-	})
-	msg_printer.PushPrivate(buf.String())
-
-	buf.Reset()
-	pub_tpl.Execute(&buf, map[string]string{
-		"cpp_type": cpp_type,
-		"pri_name": pri_name,
-		"f_name":   f.GetName(),
-	})
-	msg_printer.PushPublic(buf.String())
+`,
+			args))
 }
 
-func addRepeatedPrimitiveField(f *descriptor.FieldDescriptorProto, cpp_type string, msg_printer *MessagePrinter) {
-	const pri_template = `
-	{{.cpp_type}} {{.pri_name}};
+func addRepeatedPrimitiveField(f *descriptor.FieldDescriptorProto, type_name_info *TypeNameInfo, msg_printer *MessagePrinter) {
+	args := map[string]string{
+		"cpp_type":    type_name_info.cc_raw_type,
+		"holder_name": holderName(f),
+		"f_name":      f.GetName(),
+	}
+	msg_printer.PushPrivate(
+		print("pri_rep_pri", "    {{.cpp_type}} {{.holder_name}};\n", args))
 
-`
-	const pub_template = `
+	msg_printer.PushPublic(
+		print("pub_rep_pri", `
 	inline const {{.cpp_type}}& {{.f_name}}() const {
-	    return {{.pri_name}};
+	    return {{.holder_name}};
 	}
 
 	inline {{.cpp_type}}* mutable_{{.f_name}}() {
-		return &{{.pri_name}};
+		return &{{.holder_name}};
 	}
 
 	inline void clear_{{.f_name}}() {
-	    {{.pri_name}}.clear();
+	    {{.holder_name}}.clear();
 	}
 
-`
-
-	pri_tpl, err := template.New("pri").Parse(pri_template)
-	if err != nil {
-		log.Fatal(err)
-	}
-	pub_tpl, err := template.New("pub").Parse(pub_template)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var pri_name = f.GetName() + "__"
-	var buf bytes.Buffer
-	pri_tpl.Execute(&buf, map[string]string{
-		"cpp_type": cpp_type,
-		"pri_name": pri_name,
-	})
-	msg_printer.PushPrivate(buf.String())
-
-	buf.Reset()
-	pub_tpl.Execute(&buf, map[string]string{
-		"cpp_type": cpp_type,
-		"pri_name": pri_name,
-		"f_name":   f.GetName(),
-	})
-	msg_printer.PushPublic(buf.String())
+`,
+			args))
 }
 
-func addMessageField(f *descriptor.FieldDescriptorProto, type_name TypeNameInfo, msg_printer *MessagePrinter) {
-	const pri_template = `
-	std::unique_ptr<{{.cpp_type}}> {{.pri_name}};
+func addMessageField(f *descriptor.FieldDescriptorProto, type_name *TypeNameInfo, msg_printer *MessagePrinter) {
+	args := map[string]string{
+		"raw_type":    type_name.cc_raw_type,
+		"ret_type":    type_name.cc_ret_type,
+		"holder_name": holderName(f),
+		"f_name":      f.GetName(),
+	}
+	msg_printer.PushPrivate(
+		print("pri_rep_pri",
+			"    std::unique_ptr<{{.raw_type}}> {{.holder_name}};\n",
+			args))
 
- `
-
-	const pub_template = `
-	{{.cpp_ret_type}} {{.f_name}}() const {
-	    return *{{.pri_name}};
+	msg_printer.PushPublic(
+		print("pub_rep_pri",
+			`
+	{{.ret_type}} {{.f_name}}() const {
+	    return *{{.holder_name}};
 	}
 
-	{{.cpp_type}}* mutable_{{.f_name}}() {
-	    if (!{{.pri_name}}) {
-			{{.pri_name}} = std::make_unique<{{.cpp_type}}>();
+	{{.raw_type}}* mutable_{{.f_name}}() {
+	    if (!{{.holder_name}}) {
+			{{.holder_name}} = std::make_unique<{{.raw_type}}>();
         }
-	    return {{.pri_name}}.get();
+	    return {{.holder_name}}.get();
 	}
 
 	bool has_{{.f_name}}() const {
-	    return (bool){{.pri_name}};
+	    return (bool){{.holder_name}};
 	}
 
 	void clear_{{.f_name}}() {
-	    {{.pri_name}}.reset();
+	    {{.holder_name}}.reset();
 	}
 
- `
-
-	pri_tpl, err := template.New("pri").Parse(pri_template)
-	if err != nil {
-		log.Fatal(err)
-	}
-	pub_tpl, err := template.New("pub").Parse(pub_template)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var pri_name = f.GetName() + "__"
-	var buf bytes.Buffer
-	pri_tpl.Execute(&buf, map[string]string{
-		"cpp_type":     type_name.cc_raw_type,
-		"cpp_ret_type": type_name.cc_ret_type,
-		"pri_name":     pri_name,
-	})
-	msg_printer.PushPrivate(buf.String())
-
-	buf.Reset()
-	pub_tpl.Execute(&buf, map[string]string{
-		"cpp_type":     type_name.cc_raw_type,
-		"cpp_ret_type": type_name.cc_ret_type,
-		"pri_name":     pri_name,
-		"f_name":       f.GetName(),
-	})
-	msg_printer.PushPublic(buf.String())
+`,
+			args))
 }
