1729a7204940ea96f88237d8f614f22472d8c6a4
Naoki Iwasaki <niwasaki@f13g.com>
2024-07-03 10:50:28 +0900

    add a test for encoding/decoding enum value

--- tests/encode_decode_test.cc
+++ tests/encode_decode_test.cc
@@ -12,7 +12,7 @@ using namespace decaproto;
 
 float EPSILON = 0.0001;
 
-TEST(EncodeDecodeTest, SimplTest) {
+TEST(EncodeDecodeTest, SimpleMessageTest) {
     stringstream ss;
     StlInputStream iss(&ss);
     StlOutputStream oss(&ss);
@@ -24,23 +24,23 @@ TEST(EncodeDecodeTest, SimplTest) {
     src.mutable_other()->set_other_num(987654321);
     src.set_float_value(3.14);
     src.set_double_value(2.71828);
+    src.set_bool_value(true);
+    src.set_bool_value(false);
 
     size_t size;
+    // Encode the message to the stream.
     EXPECT_TRUE(EncodeMessage(oss, src, size));
 
+    // Decode the message from the stream.
     SimpleMessage dst;
-
-    // Compare with the default value.
-    EXPECT_NE(src.num(), dst.num());
-
     EXPECT_TRUE(DecodeMessage(iss, &dst));
 
-    // Decode the message from the stream and compare it with the original
-    // message.
+    // Let's compare the fields of the source and destination messages.
     EXPECT_EQ(src.num(), dst.num());
     EXPECT_EQ(src.str(), dst.str());
     EXPECT_EQ(src.enum_value(), dst.enum_value());
     EXPECT_EQ(src.other().other_num(), dst.other().other_num());
     EXPECT_NEAR(src.float_value(), dst.float_value(), EPSILON);
     EXPECT_NEAR(src.double_value(), dst.double_value(), EPSILON);
+    EXPECT_EQ(src.bool_value(), dst.bool_value());
 }
